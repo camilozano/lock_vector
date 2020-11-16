be305053b3b74578292df288768bfa542e314d02
Camilo Lozano <camilozano98@gmail.com>
2020-11-15 22:45:07 -0500

    Mostly working runner

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use lockvector::LockVector;
 use std::sync::Arc;
 use std::thread;
 use rand::Rng;
-use std::time::{Instant, Duration};
+use std::time::Instant;
 
 
 fn test_pushback(num_threads: i32){
@@ -36,8 +36,8 @@ fn test_pushback(num_threads: i32){
 }
 
 fn test_popback(num_threads: i32){
-    let LEN = 30;
-    let size = (num_threads as usize) * LEN;
+    let len = 30;
+    let size = (num_threads as usize) * len;
     println!("TEST POPBACK {} threads", num_threads);
 
     let v = Arc::new(LockVector::new(size));
@@ -57,7 +57,7 @@ fn test_popback(num_threads: i32){
         
         threads.push(
                 thread::spawn( move || {
-                    for j in 0..LEN{
+                    for _ in 0..len{
                         // Needs thread id and first? check :50
                         let val = v_thread.popback().unwrap();
                         good_thread.insertat(val, i as usize);
@@ -87,19 +87,19 @@ fn test_popback(num_threads: i32){
 
 fn test_cwrite(num_threads: i32){
 
-    let LEN = 44;
-    let size = LEN * (num_threads as usize);
+    let len = 44;
+    let size = len * (num_threads as usize);
 
     let v = Arc::new(LockVector::new(size));
     let mut threads = Vec::new();
-    for i in 0..LEN{
+    for _ in 0..len{
         v.pushback(0);
     }
 
     let mut cnt = Vec::new();
     for _ in 0..num_threads{
-        let new_v = Arc::new(LockVector::new(LEN));
-        for _ in 0..LEN {
+        let new_v = Arc::new(LockVector::new(len));
+        for _ in 0..len {
             new_v.pushback(0);
         }
         cnt.push(new_v);
@@ -129,10 +129,10 @@ fn test_cwrite(num_threads: i32){
         t.join().unwrap();
     }
 
-    let mut tot: Vec<usize> = vec![0;LEN];
+    let mut tot: Vec<usize> = vec![0;len];
 
     for i in 0..num_threads {
-        for j in 0..LEN {
+        for j in 0..len {
             let val = cnt[i as usize].at(j).unwrap();
             tot[j] += val;
             println!("{} ", val);
@@ -141,7 +141,7 @@ fn test_cwrite(num_threads: i32){
     }
     println!("-------------");
 
-    for i in 0..LEN{
+    for i in 0..len{
         println!("{}", v.at(i).unwrap());
     }
 
@@ -150,10 +150,151 @@ fn test_cwrite(num_threads: i32){
 }
 
 
+fn test_all(max_num_threads: usize){
+
+    let max_ops = 6400;
+    let insert = 0;
+    let erase = 1;
+    let limit = 25;
+
+    for num_threads in 1..max_num_threads {
+
+        println!("{}",num_threads);
+
+        for t in vec![insert, erase]{
+            let v = Arc::new(LockVector::new(100));
+
+            let each_thread = max_ops/num_threads;
+            let extra = max_ops % num_threads;
+            let ops_per_thread = Arc::new(LockVector::new(each_thread));
+
+            for i in 0..num_threads {
+                ops_per_thread.insertat(i, each_thread);
+                if i <= extra{
+                    ops_per_thread.addat(i, 1);
+                }
+            }
+
+            let start_time = Instant::now();
+            for i in 0..10 {
+                v.pushback(i);
+            }
+
+            let mut threads = Vec::new();
+
+            for i in 1..num_threads{
+                let thread_ops_per_thread = ops_per_thread.clone();
+                let thread_v = v.clone();
+
+                if t == insert {
+                    threads.push(
+                        thread::spawn(
+                        move || {
+                            let mut rng = rand::thread_rng();
+                            let mut r = || -> usize {
+                                rng.gen_range(0, i)
+                            };
+
+                            //TODO: Check why None value
+                            // i.e why this wasn't working
+                            //let tot_ops = thread_ops_per_thread.at(i).unwrap();
+                            let tot_ops = match thread_ops_per_thread.at(i) {
+                                Some(x) => {x},
+                                None => {1000},
+                            };
+
+                            for _ in 0..tot_ops {
+                                let cur_op = r() % 3;
+                                let do_pushack = (r()%100+100)%100 < limit;
+
+                                let x = r();
+                                let size = thread_v.size();
+                                if do_pushack {
+                                    thread_v.pushback(x);
+                                } else {
+                                    if cur_op == 0 && size > 0 {
+                                        thread_v.insertat(r() % size, x);
+                                    } else if cur_op == 1 && size > 0 {
+                                        thread_v.at(r() % size);
+                                    } else if cur_op == 2 && size > 0 {
+                                        let pos = r() % size;
+                                        let old = thread_v.at(pos).unwrap();
+                                        thread_v.cwrite(pos, old, x);
+                                    }
+                                }
+                            }
+                        }
+                    )
+                    );
+                }
+                else if t == erase {
+                    threads.push(
+                        thread::spawn(
+                            move || {
+                                let mut rng = rand::thread_rng();
+                                let mut r = || -> usize {
+                                    rng.gen_range(0, i)
+                                };
+                                //let tot_ops = thread_ops_per_thread.at(i).unwrap();
+
+                                //TODO: Check why None value
+                                let tot_ops = match thread_ops_per_thread.at(i) {
+                                    Some(x) => {x},
+                                    None => {0},
+                                };
+                                for _ in 0..tot_ops {
+                                    let cur_op = r() % 3;
+                                    let do_pushback = (r()%100+100)%100 < limit;
+
+                                    let x = r();
+                                    let size = thread_v.size();
+
+                                    if do_pushback {
+                                        thread_v.pushback(x);
+                                    } else {
+                                        if cur_op == 0 && size > 0 {
+                                            thread_v.erase(r()%size);
+                                        } else if cur_op == 1 && size > 0 {
+                                            thread_v.at(r()%size);
+                                        } else if cur_op == 2 && size > 0 {
+                                            let pos = r () % size;
+                                            let old = thread_v.at(pos).unwrap();
+                                            thread_v.cwrite(pos, old, x);
+                                        }
+                                    }
+
+
+
+                                }
+
+
+                            }
+                        )
+
+                    );
+                }
+            }
+
+            for t in threads {
+                t.join().unwrap();
+            }
+
+            let end_time = Instant::now();
+            let elapsed_time = end_time.duration_since(start_time);
+
+            println!("{:?}", elapsed_time);
+        }
+        println!("");
+    }
+
+
+
+}
+
 
 fn main(){
     let num: i32 = 10;
-    //test_all(num as usize);
+    test_all(num as usize);
     test_cwrite(num);
     test_pushback(num);
     test_popback(num);
